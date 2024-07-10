737c74ca1d20637ce78bd0d2bcaf315e1ba29c30
Rahat73 <rahat.ashik.18@gmail.com>
2024-07-10 16:13:29 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import { Application, Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+// import router from './app/routes';
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+// import notFoundHandler from './app/middlewares/notFoundHandler';
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+
+// app.use("/api", router);
+
+app.get("/", async (req: Request, res: Response) => {
+  const test = "Welcome to Sporting Goods";
+  res.send(test);
+});
+
+// app.use(globalErrorHandler);
+// app.use(notFoundHandler);
+
+export default app;
