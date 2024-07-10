c2e09f8f11d97aa82322e1f04425ec30eef872e9
Rahat73 <rahat.ashik.18@gmail.com>
2024-07-10 17:59:53 +0600

    errorhandler, middlewares, utils, interface

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express';
+
+type TResponse<T> = {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  token?: string;
+  data: T;
+};
+
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    statusCode: data.statusCode,
+    message: data.message,
+    data: data.data,
+    ...(data.token && { token: data.token }),
+  });
+};
+
+export default sendResponse;
